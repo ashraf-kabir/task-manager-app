5eb0924957e5b98f864ba73c212d1053a9800f66
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-03-23 12:21:32 +0600

    Update index.blade.php

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -7,19 +7,41 @@
 @section('content')
     <h1 class="text-center my-5">Todos Page</h1>
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-8 mb-2">
             <div class="card card-default">
-                <div class="card-header">Todos</div>
+                <div class="card-header">Pending Tasks</div>
                 <div class="card-body">
                     <ul class="list-group">
                         @foreach ($todos as $todo)
-                            <li class="list-group-item">
-                                {{ $todo->name }}
-                                @if (!$todo->completed)
-                                    <a href="/todos/{{ $todo->id }}/complete" style="color: #fff;" class="btn btn-warning btn-sm float-right">Complete</a>
+                            @if (!$todo->completed)
+                                <li class="list-group-item">
+                                    {{ $todo->name }}
+                                    <a href="/todos/{{ $todo->id }}/delete" class="btn btn-danger btn-sm float-right ml-1">Delete</a>
+                                    @if (!$todo->completed)
+                                        <a href="/todos/{{ $todo->id }}/complete" style="color: #fff;" class="btn btn-warning btn-sm float-right">Complete</a>
+                                    @endif
+                                    <a href="/todos/{{ $todo->id }}" class="btn btn-primary btn-sm float-right mr-1">View</a>
+                                </li>
+                            @endif
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-8">
+            <div class="card card-default">
+                <div class="card-header">Completed Tasks</div>
+                <div class="card-body">
+                    <ul class="list-group">
+                        @foreach ($todos as $todo)
+                                @if ($todo->completed)
+                                    <li class="list-group-item">
+                                        {{ $todo->name }}
+                                        <a href="/todos/{{ $todo->id }}/delete" class="btn btn-danger btn-sm float-right ml-1">Delete</a>
+                                        <a href="/todos/{{ $todo->id }}" class="btn btn-primary btn-sm float-right">View</a>
+                                    </li>
                                 @endif
-                                <a href="/todos/{{ $todo->id }}" class="btn btn-primary btn-sm float-right mr-2">View</a>
-                            </li>
                         @endforeach
                     </ul>
                 </div>
