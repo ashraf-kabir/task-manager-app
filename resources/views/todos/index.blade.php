de512e57bb91720474880e63f1ac47fb44e236ac
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-03-23 12:23:13 +0600

    Update index.blade.php

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -7,6 +7,7 @@
 @section('content')
     <h1 class="text-center my-5">Todos Page</h1>
     <div class="row justify-content-center">
+
         <div class="col-md-8 mb-2">
             <div class="card card-default">
                 <div class="card-header">Pending Tasks</div>
@@ -18,7 +19,7 @@
                                     {{ $todo->name }}
                                     <a href="/todos/{{ $todo->id }}/delete" class="btn btn-danger btn-sm float-right ml-1">Delete</a>
                                     @if (!$todo->completed)
-                                        <a href="/todos/{{ $todo->id }}/complete" style="color: #fff;" class="btn btn-warning btn-sm float-right">Complete</a>
+                                        <a href="/todos/{{ $todo->id }}/complete" style="color: #fff;" class="btn btn-warning btn-sm float-right">Mark Complete</a>
                                     @endif
                                     <a href="/todos/{{ $todo->id }}" class="btn btn-primary btn-sm float-right mr-1">View</a>
                                 </li>
@@ -35,17 +36,18 @@
                 <div class="card-body">
                     <ul class="list-group">
                         @foreach ($todos as $todo)
-                                @if ($todo->completed)
-                                    <li class="list-group-item">
-                                        {{ $todo->name }}
-                                        <a href="/todos/{{ $todo->id }}/delete" class="btn btn-danger btn-sm float-right ml-1">Delete</a>
-                                        <a href="/todos/{{ $todo->id }}" class="btn btn-primary btn-sm float-right">View</a>
-                                    </li>
-                                @endif
+                            @if ($todo->completed)
+                                <li class="list-group-item">
+                                    {{ $todo->name }}
+                                    <a href="/todos/{{ $todo->id }}/delete" class="btn btn-danger btn-sm float-right ml-1">Delete</a>
+                                    <a href="/todos/{{ $todo->id }}" class="btn btn-primary btn-sm float-right">View</a>
+                                </li>
+                            @endif
                         @endforeach
                     </ul>
                 </div>
             </div>
         </div>
+        
     </div>
 @endsection
