18e4f15780aca33cc3db437930ed97f563075789
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-03-23 11:51:01 +0600

    route model binding

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -13,10 +13,10 @@ class TodosController extends Controller
         return view('todos.index')->with('todos', Todo::all());
     }
 
-    public function show($todoId) {
+    public function show(Todo $todo) {
         // $todo = Todo::find($todoId);
 
-        return view('todos.show')->with('todo', Todo::find($todoId));
+        return view('todos.show')->with('todo', $todo);
     }
     
     public function create() {
@@ -42,13 +42,11 @@ class TodosController extends Controller
         return redirect('/todos');
     }
 
-    public function edit($todoId) {
-        $todo = Todo::find($todoId);
-
+    public function edit(Todo $todo) {
         return view('todos.edit')->with('todo', $todo);
     }
 
-    public function update($todoId) {
+    public function update(Todo $todo) {
         $this->validate(request(), [
             'name' => 'required|min:6|max:12',
             'description' => 'required'
@@ -56,20 +54,15 @@ class TodosController extends Controller
 
         $data = request()->all();
 
-        $todo = Todo::find($todoId);
-
         $todo->name = $data['name'];
         $todo->description = $data['description'];
-        // $todo->completed = false;
 
         $todo->save();
 
         return redirect('/todos');
     }
 
-    public function destroy($todoId) {
-        $todo = Todo::find($todoId);
-
+    public function destroy(Todo $todo) {
         $todo->delete();
         
         return redirect('/todos');
