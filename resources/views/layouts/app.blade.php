62975b0022f2464fe414c390272c4ddc9466b998
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-03-25 23:29:09 +0600

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" href="{{ url('/todos') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -35,10 +35,7 @@
                     @if (auth()->user())
                         <ul class="navbar-nav mr-auto">
                             <li class="nav-item">
-                                <a class="nav-link active" href="/new-todos">Create Todo</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" href="/todos">Todos List</a>
+                                <a class="nav-link active" href="/new-todos">Create Todos</a>
                             </li>
                         </ul>
                     @endif
